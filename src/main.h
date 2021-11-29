7c64899f60eaf383e9f43ca68e7234d05e2064e7
k-laughner <74153467+k-laughner@users.noreply.github.com>
2021-11-29 02:44:38 -0500

    Create main.h

---
+++ src/main.h
@@ -0,0 +1,9 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Kaylee Laughner
+ */
+#include <iostream> 
+#include<bits/stdc++.h>
+#include <vector>
+#include <string>
+using namespace std;
